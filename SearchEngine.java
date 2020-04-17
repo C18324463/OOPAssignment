ab5738aa9d5c83f7d2fe18b55c64b9ba0f332c54
C18324463 <55544426+C18324463@users.noreply.github.com>
2020-04-17 17:05:31 +0100

    Add files via upload

--- SearchEngine.java
+++ SearchEngine.java
@@ -29,7 +29,8 @@ public class SearchEngine extends JFrame implements ActionListener, MouseListene
 	JPanel centrePanel, topPanel, bottomPanel;
 	JTextField searchField;
 	JTextArea results;
-	JLabel programTitle;
+	JLabel programTitle, note;
+	BorderLayout layout;
 	String search;
 	File file;
 	Scanner scan;
@@ -39,30 +40,32 @@ public class SearchEngine extends JFrame implements ActionListener, MouseListene
 		super(title);
 		setSize(300, 300);
 
-		BorderLayout bl1 = new BorderLayout();
-		setLayout(bl1);
+		layout = new BorderLayout();
+		setLayout(layout);
 
 		searchButton = new JButton("Search");
 		programTitle = new JLabel("Search Engine");
+		note = new JLabel("Note: Search is case sensitive");
 		results = new JTextArea();
-		results.setEditable(false);
 		searchField = new JTextField();
-		searchField.setText("Enter Word...");
+		centrePanel = new JPanel();
+		topPanel = new JPanel();
+		bottomPanel = new JPanel();
 		
 		searchButton.setPreferredSize(new Dimension(100, 40));
 		searchField.setPreferredSize(new Dimension(130, 30));
-		results.setPreferredSize(new Dimension(100, 150));
+		results.setPreferredSize(new Dimension(150, 150));
+		results.setEditable(false);
+		searchField.setText("Enter Word...");
+		searchField.setEditable(false);
 
 		searchButton.addActionListener(this);
 		searchField.addActionListener(this);
 		searchField.addMouseListener(this);
-
-		centrePanel = new JPanel();
-		topPanel = new JPanel();
-		bottomPanel = new JPanel();
-
+		
 		topPanel.add(programTitle);
 		centrePanel.add(searchField);
+		centrePanel.add(note);
 		centrePanel.add(results);
 		bottomPanel.add(searchButton);
 
@@ -70,128 +73,143 @@ public class SearchEngine extends JFrame implements ActionListener, MouseListene
 		add(topPanel, BorderLayout.NORTH);
 		add(bottomPanel, BorderLayout.SOUTH);
 		
-
 		setVisible(true);
 	}
 
 	//Methods
-	
 	@Override
 	public void actionPerformed(ActionEvent buttonEvent) 
 	{
-		//change the path to the path that your files are in
+		//Path that your files are in
 		String path = "C:\\Users\\Gabriel Hynes\\eclipse-workspace\\C18324463 Assignment\\";
 		
 		if (buttonEvent.getSource() == searchButton) 
 		{
+			//Reading in number of files searched for size of holder array
 			int num = Integer.parseInt(JOptionPane.showInputDialog(this, "How many files are you searching?" + " Please Enter a number"));
-			results.setText(null);
 			String[] holder = new String[num];
+			//Emptying results field after each search
+			results.setText(null);
 			int j = 0;
-			try {
-			 File folder = new File(path);
-				 File[] listOfFiles = folder.listFiles();
-				 //test if there is files in list
-				 if (listOfFiles.length > 0) 
-				 {
-					//loop over length of list 
-				   for (int i = 0; i < listOfFiles.length; i++) 
-				   {
-					  //if document is a file then 
-				      if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".txt")) 
-				      {
-				    	 //read the file
-				         scan = new Scanner(new BufferedReader(new FileReader(path + listOfFiles[i].getName())));
-				         //while the file has a next line
-				         while (scan.hasNext())
-				         {	
-				        	 //Check how many words have been entered
-				        	 String text = searchField.getText();
-				        	 String [] splitter = text.trim().split("\\p{javaSpaceChar}{1,}");
-				        	 int numOfWords = splitter.length;
-				        	 if (numOfWords == 1) 
-				        	 {
-				        		 String word = scan.next();
-				        		 //equalsIgnoreCase(word); //didn't work
-					        	 if (searchField.getText().endsWith("*"))
-					        	 {
-					        		 searchField.setText("" + searchField.getText().substring(0, searchField.getText().length() - 1));
-					        		 if (word.equals(searchField.getText()))
-					        		 {
-					        			 results.append(listOfFiles[i].getName() + "\n");
-					        			 searchField.setText(searchField.getText() + "*");
-					        		 }
-					        		 else if (word.startsWith(searchField.getText())) 
-					        		 {
-				        				 holder[j] = listOfFiles[i].getName();
-				        				 j++;
-					        			 
-					        			 searchField.setText(searchField.getText() + "*");
-					        		 } else 
-					        		 {
-					        			 searchField.setText(searchField.getText() + "*");
-					        		 }
-					        		 //JOptionPane.showMessageDialog(this, word);
-					        	 }
-					        	 
-					        	 //else if word is equal to what you typed then set result field to name of file you are in
-					        	 if(word.equals(searchField.getText()))
-					        	 {
-					        		 results.append(listOfFiles[i].getName() + "\n");
-					        	 }
-				        	 } 
-				        	 if (numOfWords > 1)
-				        	 {
-				        		//using a word variable to check each word
-				        		 String word = scan.nextLine();//ONLY SCANS WORDS IF ON THE SAME LINE, FIX SO SCANS WORDS ON ANY LINE
-					        	 //else if word is equal to what you typed then set result field to name of file you are in
-					        	 if(word.contains(searchField.getText()))
-					        	 {
-					        		 results.append(listOfFiles[i].getName() + "\n");
-					        	 }
-				        	 }
-				          }//end while
-				      }//end if
-				   	}//end for
-				   for (int k = 0; k < holder.length; k++)
-				   {
-					   if (holder[k] != null)
-					   {
-						   results.append(holder[k] + "\n");
-					   }
-				   }
-				}//end 1st if
-				            
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
+			try 
+			{
+				//Putting documents in folder into an array
+				File folder = new File(path);
+				File[] listOfFiles = folder.listFiles();
+				//Test if there is files in list
+				if (listOfFiles.length > 0) 
+				{
+					//Loop over length of list 
+					for (int i = 0; i < listOfFiles.length; i++) 
+					{
+						//If document is a file and ends in .txt then 
+						if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".txt")) 
+						{
+							//Read the file
+							scan = new Scanner(new BufferedReader(new FileReader(path + listOfFiles[i].getName())));
+							//While the file has a next line
+							while (scan.hasNext())
+							{	
+								//Check how many words have been entered
+								String text = searchField.getText();
+								String [] splitter = text.trim().split("\\p{javaSpaceChar}{1,}");
+								int numOfWords = splitter.length;
+								//If only one word is being searched
+								if (numOfWords == 1) 
+								{
+									//Scan each individual word
+									String word = scan.next();
+									//Checking if its a text file
+									if (searchField.getText().endsWith("*"))
+									{
+										//Removing the *
+										searchField.setText("" + searchField.getText().substring(0, searchField.getText().length() - 1));
+										//If word scanned is an exact match to the entered word
+										if (word.equals(searchField.getText()))
+										{
+											//Put file in results field
+											results.append(listOfFiles[i].getName() + " 100% match" + "\n");
+											//Put * back on the typed word for going through the loop again
+											searchField.setText(searchField.getText() + "*");
+										}//If word scanned begins with entered word
+										else if (word.startsWith(searchField.getText())) 
+										{
+											//Put in holder array to be printed to results later
+											holder[j] = listOfFiles[i].getName();
+											j++;
+											searchField.setText(searchField.getText() + "*");
+										} else 
+										{
+											searchField.setText(searchField.getText() + "*");
+										}//End of if-else
+									}//Else if word is equal to what you typed then set result field to name of file you are in
+									else if(word.equals(searchField.getText()))
+									{
+										results.append(listOfFiles[i].getName() + " 100% match" +"\n");
+									}//End if-else
+								}//else if searching more than one word, for example 2 words or phrase/sentence
+								else if (numOfWords > 1)
+								{
+									//Using a word variable to check each line
+									String line = scan.nextLine();
+									//If line has the words/phrase entered, put file name into results field
+									if(line.contains(searchField.getText()))
+									{
+										results.append(listOfFiles[i].getName() + "\n");
+									}//End if
+								}//End of if
+							}//End while
+						}//End if
+					}//End for
+					/* For any file names in the holder array, that weren't an exact match to the typed word,
+					put those files in afterwards, as a way to rank the files in order of closest match to 
+					entered word */
+					for (int k = 0; k < holder.length; k++)
+					{
+						if (holder[k] != null)
+						{
+							results.append(holder[k] + " 75% match" + "\n");
+						}
+					}
+						}//End if          
+			} catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
 			}
+		}
+		//Close the scanner
 		scan.close();
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent event) {
+	public void mouseClicked(MouseEvent event) 
+	{
+		//When person clicks field to enter text, it clears the field and allows them to edit the field
+		searchField.setEditable(true);
 		searchField.setText("");
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
+	public void mousePressed(MouseEvent e) 
+	{
 		
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void mouseReleased(MouseEvent e) 
+	{
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseEntered(MouseEvent e) 
+	{
 		
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	public void mouseExited(MouseEvent e) 
+	{
 		
 	}
-
 }
--- SearchEngineControl.java
+++ SearchEngineControl.java
@@ -3,8 +3,7 @@ package assignment;
 public class SearchEngineControl {
 
 	public static void main(String args[]) {
+		@SuppressWarnings("unused")
 		SearchEngine mySearchEngine = new SearchEngine("New Screen");
-
 	}
-
 }
